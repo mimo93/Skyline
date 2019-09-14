b8355aca6aa613a0d566219f9ae4ab55671c3ff5
Diego Teixeira <ogeidexe@gmail.com>
2019-09-14 16:48:27 -0300

    correçao do segundo carrocel e correçao das imagens

--- sources/js/cardManipulador.js
+++ sources/js/cardManipulador.js
@@ -5,33 +5,18 @@
 * 
 * Created By Diego Teixeira dos Santos
 */
-fistSlideCreatorV2();
-function fistSlideCreator(){
-    var index;
-    //Manipulação do primeiro slide do carrosel
-    //Indicar tag.classe do carrosel Ex:div.classeTal
-    var tagDoCarrosel = "div.s1 ";
-    //Ex:div h1 header etc...
-    var elementoHtmlCard = ("cardsFirstSlide");
-    var elementoAAdicionar = elementoHtmlCard;
-    //Classes do elemento a ser adicionado Ex "classe1 classe2"
-    var classesNovoElemento = "card videoList" 
-    //Quantidade de elementos no carrocel (int)
-    var qtdElementos = 6;
-    for( index=1;index<qtdElementos+1;index++){
-        var imageTags = (elementoAAdicionar).getElementsByTagName("img");
-        var videoTags = (elementoAAdicionar).getElementsByTagName("video");
-        imageTags[0].setAttribute("src","./resources/imagens/cartoes/000"+index+".png");
-        videoTags[0].setAttribute("src","./resources/imagens/cartoes/000"+index+".mp4");
-        elemento.setAttribute("id","card"+index);
-        console.log(document.getElementById("firstSlide"))
-        $(tagDoCarrosel).append(elemento);
+var index = 2;
+index = SlideCreator(index,"div#firstCardSlide1","div#firstSlide");
+index = SlideCreator(index,"div#firstCardSlide2","div#secondSlide");
+index = SlideCreator(index,"div#firstCardSlide3","div#tirdSlide");
+function SlideCreator (index, baseElement, appendElement) {
+    var stop = index + 5;
+    for(index ; index < stop; index++){
+        elementoClonado = $(baseElement).clone();
+        console.log($(elementoClonado)[0].setAttribute("id",index));
+        imagen = $(elementoClonado)[0].getElementsByTagName("img");
+        imagen[0].setAttribute("src","./resources/imagens/cartoes/"+index+".png");
+        $(elementoClonado).appendTo(appendElement);
     }
-    //End(Manipulação do primeiro slide do carrosel)<div>
-}
-function fistSlideCreatorV2 (elements) {
-    fatherElement = document.getElementById("firstSlide");
-    sonElement = document.createElement("div");
-    sonElement.setAttribute("class","card videoList");
-    //fatherElement.append(sonElement);
+    return stop+1;
 }
