e6049906ff40d65664cd14d9d100ad31e4d533dc
Diego Teixeira <ogeidexe@gmail.com>
2019-09-14 20:40:33 -0300

    remodelagem do segundo carroucel e correçao de bugs para mobile

--- sources/js/cardManipulador.js
+++ sources/js/cardManipulador.js
@@ -6,30 +6,6 @@
 * Created By Diego Teixeira dos Santos
 */
 var index = 2;
-var backGroudCardStyleadd = [];
-
-//estilos de cada cartão
-backGroudCardStyleadd.push("#171941");
-backGroudCardStyleadd.push("linear-gradient(180deg, #2285FF -38.42%, #1A64BF 100%)");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF9F80 9.81%, #FF8D72 49.16%, #F27355 84.62%)");
-backGroudCardStyleadd.push("#E14ECA");
-backGroudCardStyleadd.push("#27AE60");
-backGroudCardStyleadd.push("#EB5757");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF6B9D 9.81%, #FD5D93 46.05%, #E65485 84.62%)");
-backGroudCardStyleadd.push("linear-gradient(180deg, #2285FF -38.42%, #1A64BF 100%)");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF9F80 9.81%, #FF8D72 49.16%, #F27355 84.62%)");
-backGroudCardStyleadd.push("#E14ECA");
-backGroudCardStyleadd.push("#27AE60");
-backGroudCardStyleadd.push("#EB5757");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF6B9D 9.81%, #FD5D93 46.05%, #E65485 84.62%)");
-backGroudCardStyleadd.push("linear-gradient(180deg, #2285FF -38.42%, #1A64BF 100%)");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF9F80 9.81%, #FF8D72 49.16%, #F27355 84.62%)");
-backGroudCardStyleadd.push("#E14ECA");
-backGroudCardStyleadd.push("#27AE60");
-backGroudCardStyleadd.push("#EB5757");
-backGroudCardStyleadd.push("linear-gradient(139.09deg, #FF6B9D 9.81%, #FD5D93 46.05%, #E65485 84.62%)");
-backGroudCardStyleadd.push("#27AE60");
-//-----
 index = SlideCreator(index,"div#firstCardSlide1","div#firstSlide");
 index = SlideCreator(index,"div#firstCardSlide2","div#secondSlide");
 index = SlideCreator(index,"div#firstCardSlide3","div#tirdSlide");
@@ -41,7 +17,6 @@ function SlideCreator (index, baseElement, appendElement) {
     for(index ; index < stop; index++){
         elementoClonado = $(baseElement).clone();
         console.log($(elementoClonado)[0].setAttribute("id",index));
-        $(elementoClonado).css("background", backGroudCardStyleadd[index-1]);
         imagen = $(elementoClonado)[0].getElementsByTagName("img");
         imagen[0].setAttribute("src","./resources/imagens/cartoes/"+index+".png");
         $(elementoClonado).appendTo(appendElement);
