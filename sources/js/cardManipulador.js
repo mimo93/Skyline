e2af71759d53928c205f9feb06ed9e0840f9e984
Diego Teixeira <ogeidexe@gmail.com>
2019-09-14 13:02:29 -0300

    ajustes de layout correçao carroceu

--- sources/js/cardManipulador.js
+++ sources/js/cardManipulador.js
@@ -5,46 +5,33 @@
 * 
 * Created By Diego Teixeira dos Santos
 */
-fistSlideCreator();
+fistSlideCreatorV2();
 function fistSlideCreator(){
     var index;
     //Manipulação do primeiro slide do carrosel
     //Indicar tag.classe do carrosel Ex:div.classeTal
-    var tagDoCarrosel = "div.view";
+    var tagDoCarrosel = "div.s1 ";
     //Ex:div h1 header etc...
-    var elementoHtmlCard = document.getElementById("cardsFirstSlide");
+    var elementoHtmlCard = ("cardsFirstSlide");
     var elementoAAdicionar = elementoHtmlCard;
     //Classes do elemento a ser adicionado Ex "classe1 classe2"
     var classesNovoElemento = "card videoList" 
     //Quantidade de elementos no carrocel (int)
     var qtdElementos = 6;
     for( index=1;index<qtdElementos+1;index++){
-        var imageTags = elementoAAdicionar.getElementsByTagName("img");
-        imageTags[0].setAttribute("src","./resources/imagens/cartoes/000"+index+".png")
-        console.log(imageTags);
-        $(tagDoCarrosel)
-        .append($(elementoAAdicionar).addClass(classesNovoElemento));
+        var imageTags = (elementoAAdicionar).getElementsByTagName("img");
+        var videoTags = (elementoAAdicionar).getElementsByTagName("video");
+        imageTags[0].setAttribute("src","./resources/imagens/cartoes/000"+index+".png");
+        videoTags[0].setAttribute("src","./resources/imagens/cartoes/000"+index+".mp4");
+        elemento.setAttribute("id","card"+index);
+        console.log(document.getElementById("firstSlide"))
+        $(tagDoCarrosel).append(elemento);
     }
     //End(Manipulação do primeiro slide do carrosel)<div>
-    secondSlideCreator(index);
 }
-function secondSlideCreator(index){
-    //Manipulação do primeiro slide do carrosel
-    //Indicar tag.classe do carrosel Ex:div.classeTal
-    var tagDoCarrosel = "div.view";
-    //Ex:div h1 header etc...
-    var elementoHtmlCard = document.getElementById("cardsFirstSlide");
-    var elementoAAdicionar = elementoHtmlCard;
-    //Classes do elemento a ser adicionado Ex "classe1 classe2"
-    var classesNovoElemento = "card videoList" 
-    //Quantidade de elementos no carrocel (int)
-    var qtdElementos = 6;
-    for( index;index<qtdElementos+1;index++){
-        var imageTags = elementoAAdicionar.getElementsByTagName("img");
-        imageTags[0].setAttribute("src","./resources/imagens/cartoes/firstSlide/000"+index+".png")
-        console.log(imageTags);
-        $("#firstSlide")
-        .append($(elementoAAdicionar).addClass(classesNovoElemento));
-    }
-    //End(Manipulação do primeiro slide do carrosel)<div>
+function fistSlideCreatorV2 (elements) {
+    fatherElement = document.getElementById("firstSlide");
+    sonElement = document.createElement("div");
+    sonElement.setAttribute("class","card videoList");
+    //fatherElement.append(sonElement);
 }
