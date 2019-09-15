b4b99b8283eb23dfe24fbf7c4a687e145b79c609
Diego Teixeira <ogeidexe@gmail.com>
2019-09-15 19:43:31 -0300

    termino da terceira parte e inicio da quarta

--- sources/js/cardManipulador.js
+++ sources/js/cardManipulador.js
@@ -5,6 +5,33 @@
 * 
 * Created By Diego Teixeira dos Santos
 */
+var cardText =[];
+cardText.push("Vídeo Lançamento");
+cardText.push("Vídeo Acompanhamento de Obra");
+cardText.push("Vídeo Time Lapse");
+cardText.push("Estande de Realidade Virtual"); 
+cardText.push("Decorado Virtual");
+cardText.push("Tour 360º");
+cardText.push("Tour Decorado 360º"); 
+cardText.push("Vídeo VT para TV");
+cardText.push("Roteiro para  Vídeos");
+cardText.push("Vídeo Empreendimento Pronto"); 
+cardText.push("Vídeo Animado");
+cardText.push("Maquete Eletrônica"); 
+cardText.push("Foto Aérea");
+cardText.push("Vídeo Subida de Andares"); 
+cardText.push("Vídeo Institucional");
+cardText.push("Vídeo Treinamento para Corretores"); 
+cardText.push("Vídeo Cartelado");
+cardText.push("Foto Arquitetônica");
+cardText.push("Vídeo Institucional");
+cardText.push("Vídeo Treinamento para Corretores");
+cardText.push("Vídeo Cartelado");
+cardText.push("Foto Arquitetônica");
+cardText.push("Vídeo Depoimento");
+
+
+
 var index = 2;
 index = SlideCreator(index,"div#firstCardSlide1","div#firstSlide");
 index = SlideCreator(index,"div#firstCardSlide2","div#secondSlide");
@@ -16,11 +43,12 @@ function SlideCreator (index, baseElement, appendElement) {
     var stop = index + 5;
     for(index ; index < stop; index++){
         elementoClonado = $(baseElement).clone();
-        console.log($(elementoClonado)[0].setAttribute("id",index));
+        $(elementoClonado)[0].setAttribute("id",index);
         imagen = $(elementoClonado)[0].getElementsByTagName("img");
+        textoTitulo = $(elementoClonado)[0].getElementsByTagName("h5");
         imagen[0].setAttribute("src","./resources/imagens/cartoes/"+index+".png");
+        textoTitulo[0].append(cardText[index]);
         $(elementoClonado).appendTo(appendElement);
     }
     return stop+1;
 }
-
